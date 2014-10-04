b4d429e10609da7b7260be6659162b3dfd66bf66
pandavenger <pandavengerx@gmail.com>
2014-10-04 01:44:05 -0400

    0143 Commit

--- simon-says/background.js
+++ simon-says/background.js
@@ -1,9 +1,16 @@
-(function () {
-	var recognition = new webkitSpeechRecognition();
-	recognition.continuous = true;
-	recognition.interimResults = true;
-	recognition.onresult = function(event) { 
-	  console.log(event) 
-	}
-	recognition.start();
-})();
+chrome.app.runtime.onLaunched.addListener(function () {
+	// Center window on screen.
+	var screenWidth = screen.availWidth;
+	var screenHeight = screen.availHeight;
+	var width = 500;
+	var height = 300;
+	chrome.app.window.create('index.html', {
+		id : "helloWorldID",
+		outerBounds : {
+			width : width,
+			height : height,
+			left : Math.round((screenWidth - width) / 2),
+			top : Math.round((screenHeight - height) / 2)
+		}
+	});
+});
