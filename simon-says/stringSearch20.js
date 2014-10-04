ddd4400d190bccfd80b18563c5937faeda6311b8
Taylor <dejesus.21@osu.edu>
2014-10-04 14:28:41 -0400

    new stringSearch method
    .js file with the new stringSearching method using indexOf()

---
+++ simon-says/stringSearch20.js
@@ -0,0 +1,22 @@
+	
+
+//var SimonMessage = chrome.runtime.connect({name: "SimonSays"})
+//SimonMessage.onMesssage.addEventListener(msg.action);
+
+
+var StringSearch = function(voiceCommands) {
+
+	//searching for close
+	if (voiceCommands.indexOf("close") > -1) {
+		//close
+		//the
+		//thing
+	}
+
+	else if (voiceCommands.indexOf("open") > -1) {
+		//open
+		//the
+		//thing
+	}	
+
+}
