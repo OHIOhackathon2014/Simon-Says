b40f82b8310afb6a8eb525415ab040023e019511
pandavenger <pandavengerx@gmail.com>
2014-10-04 02:19:10 -0400

    0218 Commit
    webkitSpeechRecognition working for raw input

---
+++ simon-says/voice.js
@@ -0,0 +1,16 @@
+var recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.onstart = function (event) {
+	document.getElementById("voiceInput").innerHTML = "Recognition Started";
+}
+recognition.onresult = function (event) {
+	var transcript = "";
+	
+	for (var i = event.resultIndex; i < event.results.length; i++) {
+		transcript += event.results[i][0].transcript;
+	}
+	
+	document.getElementById("voiceInput").innerHTML = transcript;
+}
+recognition.start();
